077ffad2e5e41a23d4794e057c507410ce578a2a
Goutham_dev <gouthamrs4u@gmail.com>
2017-11-24 00:17:06 +0530

    left sidebar design changed

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,7 +11,75 @@ class Home extends Component {
     super();
     this.state = {
       user: props.user,
-      showEmoji: false
+      showEmoji: false,
+      all_messages: [
+          {
+             image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+             name:"Anni Carpenter",
+             mesg:"Did you talk to Mark?",
+             time:"10:30 am"
+      },  {
+           image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+           name:"Anni Carpenter",
+           mesg:"Did you talk to Mark?",
+           time:"10:30 am"
+        },  {
+             image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+             name:"Anni Carpenter",
+             mesg:"Did you talk to Mark?",
+             time:"10:30 am"
+          },  {
+               image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+               name:"Anni Carpenter",
+               mesg:"Did you talk to Mark?",
+               time:"10:30 am"
+        },  {
+             image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+             name:"Anni Carpenter",
+             mesg:"Did you talk to Mark?",
+             time:"10:30 am"
+          },  {
+               image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+               name:"Anni Carpenter",
+               mesg:"Did you talk to Mark?",
+               time:"10:30 am"
+            },  {
+                 image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                 name:"Anni Carpenter",
+                 mesg:"Did you talk to Mark?",
+                 time:"10:30 am"
+              },  {
+                   image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                   name:"Anni Carpenter",
+                   mesg:"Did you talk to Mark?",
+                   time:"10:30 am"
+                },  {
+                     image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                     name:"Anni Carpenter",
+                     mesg:"Did you talk to Mark?",
+                     time:"10:30 am"
+                  },  {
+                       image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                       name:"Anni Carpenter",
+                       mesg:"Did you talk to Mark?",
+                       time:"10:30 am"
+                    },  {
+                         image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                         name:"Anni Carpenter",
+                         mesg:"Did you talk to Mark?",
+                         time:"10:30 am"
+                      },  {
+                           image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                           name:"Anni Carpenter",
+                           mesg:"Did you talk to Mark?",
+                           time:"10:30 am"
+                        },  {
+                             image:"http://i.dailymail.co.uk/i/pix/2017/04/20/13/3F6B966D00000578-4428630-image-m-80_1492690622006.jpg",
+                             name:"Anni Carpenter",
+                             mesg:"Did you talk to Mark?",
+                             time:"10:30 am"
+                          }
+      ]
     }
     this.showEmoji = this.showEmoji.bind(this);
   }
@@ -36,16 +104,30 @@ class Home extends Component {
   }
 
   render() {
-    let {user,showEmoji} = this.state;
+    let {user,showEmoji,all_messages} = this.state;
     console.log("user data is " + JSON.stringify(user));
     return (<div className="wrapper">
       {/* Sidebar Holder */}
       <nav id="sidebar" className="active">
         <div className="sidebar-header">
-          <h3>Bootstrap Sidebar</h3>
+          <h3>Lets Chat</h3>
           <strong>BS</strong>
         </div>
         <div className="nav-content">
+        {
+              all_messages.map((message,key)=>{
+                 return  <div className="left-message-box col-md-12">
+                               <div className="col-md-2 text-center">
+                                 <img src={message.image} className="img-circle" alt="Cinque Terre" width={35} height={35} />
+                               </div>
+                               <div className="col-md-8 hidden-xs">
+                                   <h3>{message.name}</h3>
+                                   <p>{message.mesg}</p>
+                               </div>
+                               <div className="col-md-2 hidden-xs"><p className="mesg-time">{message.time}</p></div>
+                            </div>
+              })
+        }
         </div>
       </nav>
       {/* Page Content Holder */}
@@ -69,47 +151,47 @@ class Home extends Component {
         </div>
         <div className="row chatbox">
             <ul className="messages">
-              <li className="message left appeared"><div className="avatar hidden-xs"/>
+              <li className="message left appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">Hello Philip! :)</div>
                 </div>
               </li>
-              <li className="message right appeared"><div className="avatar hidden-xs"/>
+              <li className="message right appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">Hi Sandy! How are you?</div>
                 </div>
               </li>
-              <li className="message left appeared"><div className="avatar hidden-xs"/>
+              <li className="message left appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">I'm fine, thank you!</div>
                 </div>
               </li>
-              <li className="message left appeared"><div className="avatar hidden-xs"/>
+              <li className="message left appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">Hello Philip! :)</div>
                 </div>
               </li>
-              <li className="message right appeared"><div className="avatar hidden-xs"/>
+              <li className="message right appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">Hi Sandy! How are you?</div>
                 </div>
               </li>
-              <li className="message left appeared"><div className="avatar hidden-xs"/>
+              <li className="message left appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">I'm fine, thank you!</div>
                 </div>
               </li>
-              <li className="message left appeared"><div className="avatar hidden-xs"/>
+              <li className="message left appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">Hello Philip! :)</div>
                 </div>
               </li>
-              <li className="message right appeared"><div className="avatar hidden-xs"/>
+              <li className="message right appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">Hi Sandy! How are you?</div>
                 </div>
               </li>
-              <li className="message left appeared"><div className="avatar hidden-xs"/>
+              <li className="message left appeared"><div className="avatar hidden-xs"> <img src="https://www.gannett-cdn.com/-mm-/1bc09ac8001ac5cd3ffef4723dba6007dbc8aeda/c=72-0-1947-2500&r=537&c=0-0-534-712/local/-/media/2016/08/10/GreatFalls/B9323321437Z.1_20160810182558_000_GFFFANB82.1-0.jpg" className="img-circle" alt="Cinque Terre" width={50} height={50} /></div>
                 <div className="text_wrapper">
                   <div className="text">I'm fine, thank you!</div>
                 </div>
